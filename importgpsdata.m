b8f2a617ae3b7003004dbcc3171508774ca6663b
CarlesG <carlesgallardollopis@gmail.com>
2022-06-02 13:03:56 +0200

    Added a commment for addpath function
    We have to change the directory path folder, where we have all the
    functions needed for export the database data.

--- importgpsdata.m
+++ importgpsdata.m
@@ -13,6 +13,7 @@ dbfilename = dir([pathname filesep '*.sqlite3']);
 if ~isempty(dbfilename)
     disp(['Loaded directory of the file ' pathname])
     % Adding to the path the database functions implemented by PAMGUARD
+    % IMPORTANT: change this line with the directory path folder where are all the database functions
     addpath('E:\Users\cargall2\ImportGPSData\pamguard-svn-r6279-MatlabCode')
 
     setdbprefs('datareturnformat', 'structure') 
