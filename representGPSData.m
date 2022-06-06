68686deab3a9d2f1a798677cfa8d354bbe4bfcb8
CarlesG <carlesgallardollopis@gmail.com>
2022-06-06 14:44:06 +0200

    Added webmap representation
    It's an interactive map representation. We need the Mapping Toolbox for
    work. It represent the transect and the start and end point, with
    latitude and longitude coordinates.

--- representGPSData.m
+++ representGPSData.m
@@ -1,4 +1,4 @@
-% Script for represent the latitud and longitud of the transect points
+%% Script for represent the latitud and longitud of the transect points
 % PARAMETERS
 % ----------
 % dbData: data structure from de GPSdata table of sqlite3 datase from 
@@ -17,3 +17,18 @@ ctab = hot(length(ix));
 for i = 1:length(ix)
     geoplot(dbData.Latitude(ix{i}), dbData.Longitude(ix{i}),'Color',ctab(i,:)), hold on
 end
+
+%% Another interactive map visualization
+% For represent the transect with Latitude and Longitude data
+wmclose("all") 
+w2 = webmap("World Imagery");
+wmline(dbData.Latitude, dbData.Longitude,'LineWidth',1,'Color','r','FeatureName','Transect','Alpha',0.99)
+zoomLevel = 9;
+
+
+% For represent the starting and ending point of the transect
+name_start = 'Start point';
+name_end = 'End point';
+wmmarker(dbData.Latitude(1),dbData.Longitude(1),'Description',[num2str(dbData.Latitude(1)),', ',num2str(dbData.Longitude(1))],'FeatureName','Start Point')
+wmmarker(dbData.Latitude(end),dbData.Longitude(end),'Description',name_end,'OverlayName',name_end)
+wmzoom(w2,zoomLevel)
